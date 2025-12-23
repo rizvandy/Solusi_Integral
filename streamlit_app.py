eacbb48d7451bc59433801a1b1e11ae4670dccd5
rizvandy <rizvandyakbar05@gmail.com>
2025-12-24 00:01:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import sympy as sp
 st.set_page_config(page_title="Kalkulator Integral", layout="centered")
 
 st.title("🧮 Kalkulator Integral Numerik")
-st.subheader("Metode Pias Titik Tengah")
+st.subheader
 
 st.write("""
 Masukkan fungsi dan batas integral untuk menghitung
@@ -14,7 +14,7 @@ aproksimasi integral menggunakan metode pias titik tengah.
 # ======================
 # INPUT USER
 # ======================
-
+fungsi_input = st.text_input("Masukkan fungsi f(x)", value="x**2")
 a = st.number_input("Batas bawah (a)", value=0.0)
 b = st.number_input("Batas atas (b)", value=1.0)
 n = st.number_input("Jumlah pias (n)", min_value=1, value=4, step=1)
