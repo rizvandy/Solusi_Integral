5c69bac53d35a27df714faa2c079e069cc2a9d5b
rizvandy <rizvandyakbar05@gmail.com>
2025-12-24 19:02:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,9 +24,12 @@ y_plot = f(x_plot)
 fig, ax = plt.subplots()
 ax.plot(x_plot, y_plot, label="f(x)")
 ax.bar(titik_tengah, nilai_f, width=h, alpha=0.3, edgecolor='black')
-
+ax.legend()
+ax.set_xlabel("x")
+ax.set_ylabel("f(x)")
 
 st.pyplot(fig)
+
 
 if st.button("Hitung Integral"):
     x = sp.symbols('x')
